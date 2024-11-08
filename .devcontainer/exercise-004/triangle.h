be5a071a1a4539440c71670c4911eeaf74707992
Manie94 <126180714+Manie94@users.noreply.github.com>
2024-11-08 10:04:18 +0100

    08.11.24_Triangle

---
+++ .devcontainer/exercise-004/triangle.h
@@ -0,0 +1,7 @@
+
+
+struct Triangle {
+    unsigned int side_a;
+    unsigned int side_b;
+    unsigned int side_c;
+}
---
+++ exercise-004/triangle.h
@@ -0,0 +1,12 @@
+//Include Guard
+#ifndef My_FANCY_TRIANGLE_H
+#define My_FANCY_TRIANGLE_H
+
+
+struct Triangle {
+    unsigned int side_a;
+    unsigned int side_b;
+    unsigned int side_c;
+};
+
+#endif
